cccebd28cd8dd6e48813c8652d2e6bd3d84e2c1d
Chloe Stokes <chloeemstokes@gmail.com>
2022-03-29 12:06:55 +0100

    Returns the middle letter on an odd word

--- middle-letter/lib/middle_letter.rb
+++ middle-letter/lib/middle_letter.rb
@@ -1,5 +1,7 @@
 def middle_letter(word)
   if word.length.even?
     word[word.length / 2 - 1] + word[word.length / 2]
+  else
+    word[word.length / 2]
   end
 end
--- middle-letter/spec/middle_letter_spec.rb
+++ middle-letter/spec/middle_letter_spec.rb
@@ -4,4 +4,11 @@ describe "#middle_letter" do
   it "returns 'es' when the input is 'test'" do
     expect(middle_letter("test")).to eq("es")
   end
+
+  it "returns 't' when the input is 'testing'" do
+    expect(middle_letter("testing")).to eq("t")
+  end
+
+  it "return 'A' when the input is 'A'" do
+  end
 end
